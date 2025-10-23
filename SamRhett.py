c1da52240a04ee28c1bef1e96d2515868e0b74d7
swpeterman <swpeterman34@gmail.com>
2025-10-23 11:56:41 -0500

    Add files via upload

---
+++ SamRhett.py
@@ -0,0 +1,13 @@
+def main():
+     
+     
+def menu():
+    print('This is the number guessing game!!')
+    print('-----------------------------------')
+    print('Menu')
+    print('-----------------------------------')
+    print('Start new game(1)')
+    print('Choose range(2)')
+    print('Exit')
+    choic
+     
